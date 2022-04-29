6be6eddcab207d26fecb3f1dc41eeb0d718d78c7
Semyon Dyachenko <semyondyachenko@gmail.com>
2022-04-29 19:05:20 +0300

    Initial Commit

---
+++ treestore/treestoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  treestoreApp.swift
+//  treestore
+//
+//  Created by Масвоок on 29.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct treestoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
